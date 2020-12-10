0010e3dae52a39abb226b57b2d4abea2763fcaa5
Jochen Kiemes <jochen@kiemes.de>
2020-12-10 09:28:48 +0100

    avr: access registers only via macro

--- examples/StepperDemo/StepperDemo.ino
+++ examples/StepperDemo/StepperDemo.ino
@@ -1,7 +1,7 @@
 #include "FastAccelStepper.h"
 #include "test_seq.h"
 
-#define VERSION "post-678ee20"
+#define VERSION "post-be8366b"
 
 struct stepper_config_s {
   uint8_t step;
--- src/StepperISR_avr.cpp
+++ src/StepperISR_avr.cpp
@@ -2,16 +2,34 @@
 #include "StepperISR.h"
 
 #if defined(ARDUINO_ARCH_AVR)
+#if !(defined(ARDUINO_AVR_NANO) || defined(ARDUINO_AVR_ATmega2560))
+#error "Unsupported board"
+#endif
+#if !(defined(__AVR_ATmega328P__) || defined(__AVR_ATmega2560__))
+#error "Unsupported AVR derivate"
+#endif
+#endif
 
-#define Stepper_Toggle(X) TCCR1A = (TCCR1A | _BV(COM1##X##0)) & ~_BV(COM1##X##1)
-#define Stepper_Zero(X) TCCR1A = (TCCR1A | _BV(COM1##X##1)) & ~_BV(COM1##X##0)
-#define Stepper_Disconnect(X) \
-  TCCR1A = (TCCR1A & ~(_BV(COM1##X##1) | _BV(COM1##X##0)))
-#define Stepper_IsToggling(X) \
-  ((TCCR1A & (_BV(COM1##X##0) | _BV(COM1##X##1))) == _BV(COM1##X##0))
-#define Stepper_IsDisconnected(X) \
-  ((TCCR1A & (_BV(COM1##X##0) | _BV(COM1##X##1))) == 0)
+#if defined(ARDUINO_ARCH_AVR)
 
+// T is the timer module number 0,1,2,3...
+// X is the Channel name A or B
+#define Stepper_Toggle(T, X) \
+  TCCR##T##A = (TCCR##T##A | _BV(COM1##X##0)) & ~_BV(COM1##X##1)
+#define Stepper_Zero(T, X) \
+  TCCR##T##A = (TCCR##T##A | _BV(COM1##X##1)) & ~_BV(COM1##X##0)
+#define Stepper_Disconnect(T, X) \
+  TCCR##T##A = (TCCR##T##A & ~(_BV(COM1##X##1) | _BV(COM1##X##0)))
+#define Stepper_IsToggling(T, X) \
+  ((TCCR##T##A & (_BV(COM1##X##0) | _BV(COM1##X##1))) == _BV(COM1##X##0))
+#define Stepper_IsDisconnected(T, X) \
+  ((TCCR##T##A & (_BV(COM1##X##0) | _BV(COM1##X##1))) == 0)
+
+#define ForceCompare(T, X) TCCR##T##C = _BV(FOC1##X)
+#define DisableCompareInterrupt(T, X) TIMSK##T &= ~_BV(OCIE1##X)
+#define EnableCompareInterrupt(T, X) TIMSK##T |= _BV(OCIE1##X)
+#define ClearInterruptFlag(T, X) TIFR##T = _BV(OCF1##X)
+#define SetTimerCompareRelative(T, X, D) OCR##T####X = TCNT##T + D
 // Here are the global variables to interface with the interrupts
 StepperQueue fas_queue[NUM_QUEUES];
 
@@ -22,17 +40,21 @@ void StepperQueue::init(uint8_t queue_num, uint8_t step_pin) {
   if (step_pin == stepPinStepperA) {
     isChannelA = true;
     noInterrupts();
-    Stepper_Disconnect(A);
-    TCCR1C = _BV(FOC1A);     // force compare to ensure disconnect
-    TIMSK1 &= ~_BV(OCIE1A);  // disable compare A interrupt
+    Stepper_Disconnect(1, A);
+    // force compare to ensure disconnect
+    ForceCompare(1, A);
+    // disable compare A interrupt
+    DisableCompareInterrupt(1, A);
     interrupts();
   }
   if (step_pin == stepPinStepperB) {
     isChannelA = false;
     noInterrupts();
-    Stepper_Disconnect(B);
-    TCCR1C = _BV(FOC1B);     // force compare to ensure disconnect
-    TIMSK1 &= ~_BV(OCIE1B);  // disable compare B interrupt
+    Stepper_Disconnect(1, B);
+    // force compare to ensure disconnect
+    ForceCompare(1, B);
+    // disable compare B interrupt
+    DisableCompareInterrupt(1, B);
     interrupts();
   }
 }
@@ -40,8 +62,9 @@ void StepperQueue::init(uint8_t queue_num, uint8_t step_pin) {
 #define AVR_STEPPER_ISR(CHANNEL, queue, ocr, foc)                     \
   ISR(TIMER1_COMP##CHANNEL##_vect) {                                  \
     uint8_t rp = queue.read_idx;                                      \
-    if (Stepper_IsToggling(CHANNEL)) {                                \
-      TCCR1C = _BV(foc); /* clear bit */                              \
+    if (Stepper_IsToggling(1, CHANNEL)) {                             \
+      /* Clear output bit by another toggle */                        \
+      ForceCompare(1, CHANNEL);                                       \
       struct queue_entry* e = &queue.entry[rp & QUEUE_LEN_MASK];      \
       if (e->steps-- > 0) {                                           \
         /* perform another steps_dir with this queue entry */         \
@@ -50,17 +73,17 @@ void StepperQueue::init(uint8_t queue_num, uint8_t step_pin) {
       }                                                               \
       rp++;                                                           \
       queue.read_idx = rp;                                            \
-    } else if (!Stepper_IsDisconnected(CHANNEL)) {                    \
+    } else if (!Stepper_IsDisconnected(1, CHANNEL)) {                 \
       rp++;                                                           \
       queue.read_idx = rp;                                            \
     }                                                                 \
     if (rp == queue.next_write_idx) {                                 \
       /* queue is empty => set to disconnect */                       \
-      Stepper_Disconnect(CHANNEL);                                    \
+      Stepper_Disconnect(1, CHANNEL);                                 \
       /* disable compare interrupt */                                 \
-      TIMSK1 &= ~_BV(OCIE1##CHANNEL);                                 \
+      DisableCompareInterrupt(1, CHANNEL);                            \
       /* force compare to ensure disconnect */                        \
-      TCCR1C = _BV(FOC1##CHANNEL);                                    \
+      ForceCompare(1, CHANNEL);                                       \
       queue.isRunning = false;                                        \
       queue.queue_end.ticks = TICKS_FOR_STOPPED_MOTOR;                \
       return;                                                         \
@@ -70,9 +93,9 @@ void StepperQueue::init(uint8_t queue_num, uint8_t step_pin) {
     ocr += (queue.ticks = e->ticks);                                  \
     /* assign to skip and test for not zero */                        \
     if (e->steps == 0) {                                              \
-      Stepper_Zero(CHANNEL);                                          \
+      Stepper_Zero(1, CHANNEL);                                       \
     } else {                                                          \
-      Stepper_Toggle(CHANNEL);                                        \
+      Stepper_Toggle(1, CHANNEL);                                     \
     }                                                                 \
     if (e->toggle_dir) {                                              \
       uint8_t dirPin = queue.dirPin;                                  \
@@ -86,33 +109,41 @@ void StepperQueue::startQueue() {
   isRunning = true;
   if (isChannelA) {
     noInterrupts();
-    TIFR1 = _BV(OCF1A);     // clear interrupt flag
-    TIMSK1 |= _BV(OCIE1A);  // enable compare A interrupt
-    OCR1A = TCNT1 + 40;     // definite start point
+    TIFR1 = _BV(OCF1A);
+    // clear interrupt flag
+    ClearInterruptFlag(1, A);
+    // enable compare A interrupt
+    EnableCompareInterrupt(1, A);
+    OCR1A = TCNT1 + 40;
+    // definite start point
+    SetTimerCompareRelative(1, A, 40);
     interrupts();
   } else {
     noInterrupts();
-    TIFR1 = _BV(OCF1B);     // clear interrupt flag
-    TIMSK1 |= _BV(OCIE1B);  // enable compare A interrupt
-    OCR1B = TCNT1 + 40;     // definite start point
+    // clear interrupt flag
+    ClearInterruptFlag(1, B);
+    // enable compare B interrupt
+    EnableCompareInterrupt(1, B);
+    // definite start point
+    SetTimerCompareRelative(1, B, 40);
     interrupts();
   }
 }
 void StepperQueue::forceStop() {
   if (isChannelA) {
     /* disable compare interrupt */
-    TIMSK1 &= ~_BV(OCIE1A);
+    DisableCompareInterrupt(1, A);
     /* set to disconnect */
-    Stepper_Disconnect(A);
+    Stepper_Disconnect(1, A);
     /* force compare to ensure disconnect */
-    TCCR1C = _BV(FOC1A);
+    ForceCompare(1, A);
   } else {
     /* disable compare interrupt */
-    TIMSK1 &= ~_BV(OCIE1B);
+    DisableCompareInterrupt(1, B);
     /* set to disconnect */
-    Stepper_Disconnect(B);
+    Stepper_Disconnect(1, B);
     /* force compare to ensure disconnect */
-    TCCR1C = _BV(FOC1B);
+    ForceCompare(1, B);
   }
   isRunning = false;
   queue_end.ticks = TICKS_FOR_STOPPED_MOTOR;
