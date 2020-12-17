fd6201073c2faa8a855dce4dce343d9e459c8b49
Jochen Kiemes <jochen@kiemes.de>
2020-12-17 17:52:06 +0100

    fix test sequence 05

--- examples/StepperDemo/test_seq_05.cpp
+++ examples/StepperDemo/test_seq_05.cpp
@@ -4,6 +4,7 @@
 //
 // Perform 800 times a single step and then 800 steps back in one command.
 
+#define DT 200
 bool test_seq_05(FastAccelStepper *stepper, struct test_seq_s *seq, uint32_t time_ms) {
 	switch (seq->state & 15) {
 		case 0: // INIT
@@ -13,7 +14,7 @@ bool test_seq_05(FastAccelStepper *stepper, struct test_seq_s *seq, uint32_t tim
 			seq->state++;
 			break;
 		case 1: // Wait 1s pass
-			if (time_ms - seq->u32_1 >= 500) {
+			if (time_ms - seq->u32_1 >= DT) {
 				if ((seq->state & 0xfff0) == (16*800)) {
 					seq->state = 7;
 					stepper->setSpeed(1000);
@@ -26,9 +27,9 @@ bool test_seq_05(FastAccelStepper *stepper, struct test_seq_s *seq, uint32_t tim
 			break;
 		case 2:
 			// second pass, run motor
-			seq->u32_1 += 500;
+			seq->u32_1 += DT;
 			stepper->move(1);
-			seq->state = 1;
+			seq->state += 16-1;
 			break;
 		case 7:
 			if (!stepper->isRunning()) {
