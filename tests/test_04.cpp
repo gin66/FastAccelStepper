9002f7a826faa4f250d7b36a87dceb2ea5fb5cec
Jochen Kiemes <jochen@kiemes.de>
2021-01-01 19:07:23 +0100

    add failing test for issue #23

--- tests/test_04.cpp
+++ tests/test_04.cpp
@@ -28,7 +28,8 @@ void init_queue() {
   fas_queue[1].next_write_idx = 0;
 }
 
-int main() {
+void test_speed_increase() {
+  puts("Test test_speed_increase");
   init_queue();
   FastAccelStepper s = FastAccelStepper();
   s.init(NULL, 0, 0);
@@ -87,6 +88,78 @@ int main() {
 #if (TEST_CREATE_QUEUE_CHECKSUM == 1)
   printf("CHECKSUM for %d/%d/%d: %d\n", steps, travel_dt, accel, s.checksum);
 #endif
+}
 
+void test_speed_decrease() {
+  puts("Test test_speed_decrease");
+  init_queue();
+  FastAccelStepper s = FastAccelStepper();
+  s.init(NULL, 0, 0);
+  RampChecker rc = RampChecker();
+  assert(0 == s.getCurrentPosition());
+
+  int32_t steps = 10000;
+
+  // Increase speed to 400, then further to 300
+  // Identified bug was a fast jump to 300 without acceleration
+  assert(s.isQueueEmpty());
+  s.setSpeed(400);
+  s.setAcceleration(1000);
+  s.fill_queue();
+  assert(s.isQueueEmpty());
+  s.move(steps);
+  s.fill_queue();
+  assert(!s.isQueueEmpty());
+  float old_planned_time_in_buffer = 0;
+  int speed_decreased = false;
+  uint32_t count_state_dec = 0;
+  for (int i = 0; i < steps; i++) {
+    if (!speed_decreased && (s.getCurrentPosition() >= 5000)) {
+      puts("Change speed");
+      s.fill_queue();  // ensure queue is not empty
+      speed_decreased = true;
+      s.setSpeed(500);
+      s.move(steps);
+    }
+    if (true) {
+      printf(
+          "Loop %d: Queue read/write = %d/%d    Target pos = %d, Queue End "
+          "pos = %d  QueueEmpty=%s\n",
+          i, fas_queue[0].read_idx, fas_queue[0].next_write_idx, s.targetPos(),
+          s.getPositionAfterCommandsCompleted(),
+          s.isQueueEmpty() ? "yes" : "no");
+    }
+    if (!s.isRampGeneratorActive()) {
+      break;
+    }
+    s.fill_queue();
+	if (s.rampState() == RAMP_STATE_DECELERATE) {
+	  count_state_dec++;
+	}
+    uint32_t from_dt = rc.total_ticks;
+    while (!s.isQueueEmpty()) {
+      rc.check_section(
+          &fas_queue[0].entry[fas_queue[0].read_idx & QUEUE_LEN_MASK]);
+      fas_queue[0].read_idx++;
+    }
+    uint32_t to_dt = rc.total_ticks;
+    float planned_time = (to_dt - from_dt) * 1.0 / 16000000;
+    printf("planned time in buffer: %.6fs\n", planned_time);
+    // This must be ensured, so that the stepper does not run out of commands
+    assert((i == 0) || (old_planned_time_in_buffer > 0.005));
+    old_planned_time_in_buffer = planned_time;
+  }
+  test(!s.isRampGeneratorActive(), "too many commands created");
+  test(count_state_dec > 10, "no deceleration to new speed");
+  printf("Total time  %f\n", rc.total_ticks / 16000000.0);
+#if (TEST_CREATE_QUEUE_CHECKSUM == 1)
+  printf("CHECKSUM for %d/%d/%d: %d\n", steps, travel_dt, accel, s.checksum);
+#endif
+}
+
+int main() {
+  test_speed_increase();
+  test_speed_decrease();
   printf("TEST_04 PASSED\n");
+  return 0;
 }
