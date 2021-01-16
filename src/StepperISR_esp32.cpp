583aa861f796c57211c1a04e23de4345fd18232b
Jochen Kiemes <jochen@kiemes.de>
2021-01-16 11:03:34 +0100

    not hardcoded to pulse counter 7

--- src/StepperISR_esp32.cpp
+++ src/StepperISR_esp32.cpp
@@ -388,12 +388,29 @@ bool StepperQueue::isValidStepPin(uint8_t step_pin) { return true; }
 int8_t StepperQueue::queueNumForStepPin(uint8_t step_pin) { return -1; }
 
 
+uint32_t sig_idx[8] = {
+	PCNT_SIG_CH0_IN0_IDX,
+	PCNT_SIG_CH0_IN1_IDX,
+	PCNT_SIG_CH0_IN2_IDX,
+	PCNT_SIG_CH0_IN3_IDX,
+	PCNT_SIG_CH0_IN4_IDX,
+	PCNT_SIG_CH0_IN5_IDX,
+	PCNT_SIG_CH0_IN6_IDX,
+	PCNT_SIG_CH0_IN7_IDX
+};
+uint32_t ctrl_idx[8] = {
+	PCNT_CTRL_CH0_IN0_IDX,
+	PCNT_CTRL_CH0_IN1_IDX,
+	PCNT_CTRL_CH0_IN2_IDX,
+	PCNT_CTRL_CH0_IN3_IDX,
+	PCNT_CTRL_CH0_IN4_IDX,
+	PCNT_CTRL_CH0_IN5_IDX,
+	PCNT_CTRL_CH0_IN6_IDX,
+	PCNT_CTRL_CH0_IN7_IDX
+};
 
 bool _esp32_attachToPulseCounter(uint8_t pcnt_unit, FastAccelStepper *stepper) {
-	// TODO: Check if free pulse counter
-  if (pcnt_unit < 7) { // 7 is still hard coded here
-	  return false;
-  }
+  // TODO: Check if free pulse counter
 
   pcnt_config_t cfg;
   uint8_t dir_pin = stepper->getDirectionPin();
@@ -414,7 +431,7 @@ bool _esp32_attachToPulseCounter(uint8_t pcnt_unit, FastAccelStepper *stepper) {
   cfg.neg_mode = PCNT_COUNT_DIS;  // ignore falling edge
   cfg.counter_h_lim = 16384;
   cfg.counter_l_lim = -16384;
-  cfg.unit = PCNT_UNIT_7;
+  cfg.unit = (pcnt_unit_t)pcnt_unit;
   cfg.channel = PCNT_CHANNEL_0;
   pcnt_unit_config(&cfg);
 
@@ -426,15 +443,15 @@ bool _esp32_attachToPulseCounter(uint8_t pcnt_unit, FastAccelStepper *stepper) {
 
   stepper->detachFromPin();
   stepper->reAttachToPin();
-  gpio_iomux_in(stepper->getStepPin(), PCNT_SIG_CH0_IN7_IDX);
+  gpio_iomux_in(stepper->getStepPin(), sig_idx[pcnt_unit]);
   if (dir_pin != PIN_UNDEFINED) {
     gpio_matrix_out(stepper->getDirectionPin(), 0x100, false, false);
-    gpio_iomux_in(stepper->getDirectionPin(), PCNT_CTRL_CH0_IN7_IDX);
+    gpio_iomux_in(stepper->getDirectionPin(), ctrl_idx[pcnt_unit]);
     pinMode(stepper->getDirectionPin(), OUTPUT);
   }
   return true;
 }
 int16_t _esp32_readPulseCounter(uint8_t pcnt_unit) {
-  return PCNT.cnt_unit[PCNT_UNIT_7].cnt_val;
+  return PCNT.cnt_unit[(pcnt_unit_t)pcnt_unit].cnt_val;
 }
 #endif
