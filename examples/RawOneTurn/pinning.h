01f35f48c4d715db7060b61f0b3d670cc7d347e4
Jochen Kiemes <jochen@kiemes.de>
2022-01-24 19:57:39 +0100

    github ci pointed out the missing file pinning.h

---
+++ examples/RawOneTurn/pinning.h
@@ -0,0 +1,18 @@
+#include "FastAccelStepper.h"
+
+#if defined(ARDUINO_ARCH_AVR)
+#include "AVRStepperPins.h"
+#define vTaskDelay(xx) {}
+#define stepPinStepper stepPinStepper1A
+#define enablePinStepper 6
+#define dirPinStepper 5
+#elif defined(ARDUINO_ARCH_ESP32)
+#define stepPinStepper 17
+#define enablePinStepper 26
+#define dirPinStepper 18
+#else
+#define vTaskDelay(xx) {}
+#define stepPinStepper 17
+#define enablePinStepper 26
+#define dirPinStepper 18
+#endif
