5187767b532dec9c9c97afb0e89180c060f4f730
Jochen Kiemes <jochen@kiemes.de>
2022-02-06 16:01:30 +0100

    make esp32_hw_based test seq_01 with M7 compatible

--- tests/esp32_hw_based/seq_01.sh
+++ tests/esp32_hw_based/seq_01.sh
@@ -3,8 +3,8 @@ TTY=${1:-ttyUSB0}
 DEV="-d /dev/${TTY} -b 115200"
 MOTOR=${MOTOR:-M1}
 
-CMD="M1 p7,0,0 H25000 A10000 f w1000 X pc R100 w100 W R1000 "
-PASS=">> M1: @1100 \\[1100\\]"
+CMD="$MOTOR p7,0,0 H25000 A10000 f w1000 X pc R100 w100 W R1000 "
+PASS=">> $MOTOR: @1100 \\[1100\\]"
 
 LOG="$0.log"
 
