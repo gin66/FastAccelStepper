f94d61b2c707ed09aeab1dd3d65faacab9a72ec0
Jochen Kiemes <jochen@kiemes.de>
2022-02-06 13:36:05 +0100

    fix test sequence 13

--- examples/StepperDemo/test_seq_13.cpp
+++ examples/StepperDemo/test_seq_13.cpp
@@ -5,6 +5,7 @@
 
 bool test_seq_13(FastAccelStepper *stepper, struct test_seq_s *seq,
                  uint32_t time_ms) {
+  uint8_t res;
   struct stepper_command_s cmd_step = {
       .ticks = 50000, .steps = 1, .count_up = true};
   switch (seq->state) {
@@ -14,9 +15,15 @@ bool test_seq_13(FastAccelStepper *stepper, struct test_seq_s *seq,
     case 1:
     case 2:
     case 3:
-      stepper->addQueueEntry(&cmd_step);
-      seq->u32_1 = time_ms;
-      seq->state++;
+      res = stepper->addQueueEntry(&cmd_step);
+	  if (res > 1) {
+	     Serial.print(res);
+		 Serial.print(' ');
+	  }
+	  else {
+         seq->u32_1 = time_ms;
+         seq->state++;
+	  }
       break;
     case 4:
       if (time_ms - seq->u32_1 >= 20) {
