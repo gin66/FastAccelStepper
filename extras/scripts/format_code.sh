ce9c49d6c32434510cad689dd3d5ba1a7caa00c6
Jochen Kiemes <jochen@kiemes.de>
2022-06-25 18:42:52 +0200

    adjust format code script

--- extras/scripts/format_code.sh
+++ extras/scripts/format_code.sh
@@ -3,9 +3,9 @@
 PRJ_ROOT=`git rev-parse --show-toplevel`
 VERSION=`git rev-parse --short HEAD`
 
-FILES=`find ${PRJ_ROOT} -type f -name '*.ino' -or -name '*.cpp' -or -name '*.h'`
+FILES=`find ${PRJ_ROOT} -type f -name '*.ino' -or -type f -name '*.cpp' -or -type f -name '*.h'`
 
-clang-format --style=Google -i ${FILES}
+clang-format -style="{BasedOnStyle: Google, SortIncludes: false}" -i ${FILES}
 echo ${VERSION}
 sed -i -e 's/#define VERSION.*$$/#define VERSION "post-$(VERSION)"/' ${PRJ_ROOT}/examples/StepperDemo/StepperDemo.ino
 
