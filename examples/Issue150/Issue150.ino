12980fc9477f0b71506248b33f7c2f4a0a399e3b
Jochen Kiemes <jochen@kiemes.de>
2022-11-12 09:41:41 +0100

    add pass/fail criteria

--- examples/Issue150/Issue150.ino
+++ examples/Issue150/Issue150.ino
@@ -17,14 +17,47 @@ void setup() {
     stepper->setDirectionPin(dirPinStepper);
 	stepper->setEnablePin(enablePinStepper, true);
 	stepper->setAutoEnable(true);
-    stepper->setAcceleration(1000000);
+    stepper->setAcceleration(1000);
 }
 
+uint16_t loopCnt = 0;
+
 void loop() {
+    loopCnt++;
     int targetSpeed = rand() % 40000 + 100;
     stepper->setSpeedInUs(targetSpeed);  // the parameter is us/step !!!
     stepper->runForward();
     delay(500);
+	Serial.printf("Loop: %d\n", loopCnt);
     Serial.printf("target: %d\n", targetSpeed);
     Serial.printf("actual: %d\n", stepper->getCurrentSpeedInUs());
+    if (loopCnt == 10) {
+		bool pass = true;
+		stepper->setSpeedInUs(1000);  // the parameter is us/step !!!
+		stepper->applySpeedAcceleration();
+        delay(3000);
+        if (1000 != stepper->getCurrentSpeedInUs()) {
+			pass = false;
+			Serial.printf("target: %d\n", targetSpeed);
+			Serial.printf("actual: %d\n", stepper->getCurrentSpeedInUs());
+		}
+		stepper->setSpeedInUs(500);  // the parameter is us/step !!!
+		stepper->applySpeedAcceleration();
+        delay(3000);
+        if (500 != stepper->getCurrentSpeedInUs()) {
+			pass = false;
+			Serial.printf("target: %d\n", targetSpeed);
+			Serial.printf("actual: %d\n", stepper->getCurrentSpeedInUs());
+		}
+		stepper->stopMove();
+		while(true) {
+			if (pass) {
+				Serial.printf("PASS");
+			}
+			else {
+				Serial.printf("FAIL");
+			}
+			delay(1000);
+		}
+    }
 }
