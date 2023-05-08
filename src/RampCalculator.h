0a64255b1b2ee3bc6315d4043534421e523bdf61
Jochen Kiemes <jochen@kiemes.de>
2023-05-08 19:06:35 +0200

    move ramp_config_s to RampCalculator.h

--- src/RampCalculator.h
+++ src/RampCalculator.h
@@ -1,3 +1,6 @@
+#ifndef RAMP_CALCULATOR_H
+#define RAMP_CALCULATOR_H
+
 #include <stdint.h>
 
 #include "PoorManFloat.h"
@@ -16,3 +19,47 @@ uint32_t calculate_ticks_v8(uint32_t steps, pmf_logarithmic pre_calc);
 
 uint32_t calculate_ticks(uint32_t steps, pmf_logarithmic pmfl_accel);
 uint32_t calculate_ramp_steps(uint32_t ticks, pmf_logarithmic pmfl_accel);
+
+struct ramp_config_s {
+  uint32_t min_travel_ticks;
+  uint32_t max_ramp_up_steps;
+  pmf_logarithmic pmfl_accel;
+  uint8_t accel_change_cnt;
+
+  void init() {
+    accel_change_cnt = 0;
+    min_travel_ticks = 0;
+    max_ramp_up_steps = 0;
+    pmfl_accel = PMF_CONST_INVALID;
+  }
+  inline int8_t checkValidConfig() {
+    if (min_travel_ticks == 0) {
+      return MOVE_ERR_SPEED_IS_UNDEFINED;
+    }
+    if (pmfl_accel == PMF_CONST_INVALID) {
+      return MOVE_ERR_ACCELERATION_IS_UNDEFINED;
+    }
+    return MOVE_OK;
+  }
+  inline void setSpeedInTicks(uint32_t min_step_ticks) {
+    if (min_travel_ticks != min_step_ticks) {
+      min_travel_ticks = min_step_ticks;
+      if (checkValidConfig() == MOVE_OK) {
+        max_ramp_up_steps = calculate_ramp_steps(min_step_ticks, pmfl_accel);
+      }
+    }
+  }
+  inline void setAcceleration(int32_t accel) {
+    pmf_logarithmic new_pmfl_accel = pmfl_from((uint32_t)accel);
+    if (pmfl_accel != new_pmfl_accel) {
+      pmfl_accel = new_pmfl_accel;
+
+      if (checkValidConfig() == MOVE_OK) {
+        max_ramp_up_steps = calculate_ramp_steps(min_travel_ticks, pmfl_accel);
+      }
+      accel_change_cnt++;
+    }
+  }
+};
+
+#endif
