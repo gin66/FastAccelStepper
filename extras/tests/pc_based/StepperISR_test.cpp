d10111e6e4de411a2aa5cc6f59e12a0499481dec
Jochen Kiemes <jochen@kiemes.de>
2024-12-24 18:10:28 +0100

    fix compile error for pc-based tests

--- extras/tests/pc_based/StepperISR_test.cpp
+++ extras/tests/pc_based/StepperISR_test.cpp
@@ -5,7 +5,7 @@
 // Here are the global variables to interface with the interrupts
 // StepperQueue fas_queue[NUM_QUEUES];
 
-void fas_init_engine(FastAccelStepperEngine* engine, uint8_t cpu_core) {}
+void fas_init_engine(FastAccelStepperEngine* engine) {}
 
 void StepperQueue::init(uint8_t queue_num, uint8_t step_pin) { _initVars(); }
 void StepperQueue::startQueue() { _isRunning = true; }
