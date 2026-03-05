67d2dd47e77e13606dc609a51df6dc5ec45a1e23
Jochen Kiemes <jochen@kiemes.de>
2026-03-05 09:58:22 +0100

    Refactor: Split FastAccelStepperEngine into separate files
    Move FastAccelStepperEngine class declaration and implementation from
    FastAccelStepper.{h,cpp} into dedicated FastAccelStepperEngine.{h,cpp}
    files. This improves code organization and reduces file sizes.
    
    The split includes:
    - Class declaration in FastAccelStepperEngine.h
    - Implementation and global variables in FastAccelStepperEngine.cpp
    - Updated FastAccelStepper.h to include the new header
    - Updated test Makefile to build the new source file

---
+++ src/FastAccelStepperEngine.h
@@ -0,0 +1,69 @@
+#ifndef FASTACCELSTEPPER_ENGINE_H
+#define FASTACCELSTEPPER_ENGINE_H
+#include <stdint.h>
+#include "fas_arch/common.h"
+
+class FastAccelStepper;
+
+class FastAccelStepperEngine {
+ public:
+#if defined(SUPPORT_CPU_AFFINITY)
+  void init(uint8_t cpu_core = 255);
+#else
+  void init();
+#endif
+
+#if !defined(SUPPORT_SELECT_DRIVER_TYPE)
+  FastAccelStepper* stepperConnectToPin(uint8_t step_pin);
+#endif
+#if defined(SUPPORT_SELECT_DRIVER_TYPE)
+#define DRIVER_MCPWM_PCNT FasDriver::MCPWM_PCNT
+#define DRIVER_RMT FasDriver::RMT
+#if defined(SUPPORT_ESP32_I2S)
+#define DRIVER_RMT_I2S_DIRECT FasDriver::RMT_I2S_DIRECT
+#define DRIVER_RMT_I2S_MUX FasDriver::RMT_I2S_MUX
+#endif
+#define DRIVER_DONT_CARE FasDriver::DONT_CARE
+  FastAccelStepper* stepperConnectToPin(
+      uint8_t step_pin, FasDriver driver_type = DRIVER_DONT_CARE);
+#endif
+
+#if defined(SUPPORT_TASK_RATE_CHANGE)
+  inline void task_rate(uint8_t delay_ms) { _delay_ms = delay_ms; };
+  uint8_t _delay_ms;
+#endif
+
+  void setExternalCallForPin(bool (*func)(uint8_t pin, uint8_t value));
+  void setDebugLed(uint8_t ledPin);
+
+#if defined(SUPPORT_ESP32_I2S)
+  struct I2sSingleStepperConfig {
+    uint8_t data_pin;
+    uint8_t bclk_pin;
+    uint8_t cpu_core;
+    uint16_t pulse_width_ticks;
+  };
+  bool initI2sSingleStepper(const I2sSingleStepperConfig& cfg);
+  bool isI2sInitialized() const;
+#endif
+
+  void manageSteppers();
+
+ private:
+  bool isDirPinBusy(uint8_t dirPin, uint8_t except_stepper);
+
+  uint8_t _stepper_cnt;
+  FastAccelStepper* _stepper[MAX_STEPPER];
+  
+  bool (*_externalCallForPin)(uint8_t pin, uint8_t value);
+
+#if defined(SUPPORT_RP_PICO)
+  uint8_t claimed_pios;
+  PIO pio[NUM_PIOS];
+#endif
+
+  friend class FastAccelStepper;
+  friend class StepperQueue;
+};
+
+#endif
