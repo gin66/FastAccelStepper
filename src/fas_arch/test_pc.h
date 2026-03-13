f606a5885b2a1b9eccf9b318cdcb7f4dd27d137e
Jochen Kiemes <jochen@kiemes.de>
2026-03-13 17:42:59 +0100

    move stubs.h to fas_arch/test_pc.h

--- src/fas_arch/test_pc.h
+++ src/fas_arch/test_pc.h
@@ -9,7 +9,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "../extras/tests/pc_based/stubs.h"
+// Arduino/AVR compatibility stubs for PC-based testing
+#define PROGMEM
+#define pgm_read_byte_near(x) (*(x))
+
+// For inducing interrupts while testing
+void noInterrupts();
+void interrupts();
+void inject_fill_interrupt(int mark);
+
+#define _BV(x) 0
+#define ISR(x) void x()
+#define inline
+#define micros() 0
+
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define min(a, b) ((a) > (b) ? (b) : (a))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
+#define digitalWrite(a, b) \
+  {                        \
+  }
+#define pinMode(a, b) \
+  {                   \
+  }
+
+extern char TCCR1A;
+extern char TCCR1B;
+extern char TCCR1C;
+extern char TIMSK1;
+extern char TIFR1;
+extern unsigned short OCR1A;
+extern unsigned short OCR1B;
+
+#define test(x, msg) \
+  if (!(x)) {        \
+    puts(msg);       \
+    assert(false);   \
+  };
 
 // For pc-based testing, the macro TEST is defined. The pc-based testing does
 // not support the concept of interrupts, so provide an empty definition
