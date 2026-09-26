3ecdd07d4cd604657eaf6374c90dd976651811c0
Jochen Kiemes <jochen@kiemes.de>
2026-09-26 17:56:34 +0200

    ci: trigger workflows on naxes branch too

--- extras/scripts/generate_build_configs.py
+++ extras/scripts/generate_build_configs.py
@@ -228,12 +228,11 @@ def generate_workflow_yml(
     if "on" in workflow_config:
         lines.append("on:")
         on_config = workflow_config["on"]
-        if "push" in on_config:
-            lines.append("  push:")
-            lines.append("    branches: [ master ]")
-        if "pull_request" in on_config:
-            lines.append("  pull_request:")
-            lines.append("    branches: [ master ]")
+        for event in ("push", "pull_request"):
+            if event in on_config:
+                branches = on_config[event].get("branches", ["master"])
+                lines.append(f"  {event}:")
+                lines.append(f"    branches: [ {', '.join(branches)} ]")
         lines.append("")
 
     lines.extend(
